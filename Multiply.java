c87493cb9d86220a9489d82ec305dd96dafbb23e
Asutosh <asutoshpattajoshi01@gmail.com>
2023-08-21 10:59:09 +0530

    Multiplication

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class ProductCalculator {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 3;
+        
+        int product = num1 * num2;
+        
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
