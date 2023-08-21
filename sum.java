22d87b5cb6a12524c25cfc71b96ba8566a04d358
Asutosh <asutoshpattajoshi01@gmail.com>
2023-08-21 10:45:14 +0530

    Adding Sum.java

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ public class SumCalculator {
         int num1 = 7;
         int num2 = 8;
         
-        int sum = num1 + num2;
+        int sum = num1  and + num2;
         
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
