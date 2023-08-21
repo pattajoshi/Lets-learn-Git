2a992252cba6227f1f71cb896aefce4d27e940b5
Asutosh <asutoshpattajoshi01@gmail.com>
2023-08-21 10:33:34 +0530

    In

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class SumCalculator {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 8;
+        
+        int sum = num1 + num2;
+        
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
